7decf6239423d4bdd67a73fac2e99b935ccc3ca9
Alan Willms <dyulax@gmail.com>
2021-02-21 23:01:11 -0300

    navigation buckets

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import {Scene, Router} from 'react-native-router-flux';
+import {Scene, Router, Actions} from 'react-native-router-flux';
+import EmployeeCreate from './components/EmployeeCreate';
 import EmployeeList from './components/EmployeeList';
 import LoginForm from './components/LoginForm';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{paddingTop: 65}}>
-      <Scene key="root">
-        <Scene key="login" component={LoginForm} title="Please Login" initial />
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      <Scene key="root" hideNavBar>
+        <Scene key="auth">
+          <Scene
+            key="login"
+            component={LoginForm}
+            title="Please Login"
+            initial
+          />
+        </Scene>
+        <Scene key="main">
+          <Scene
+            rightTitle="Add"
+            onRight={() => Actions.employeeCreate()}
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+          />
+
+          <Scene
+            key="employeeCreate"
+            title="Create Employee"
+            component={EmployeeCreate}
+          />
+        </Scene>
       </Scene>
     </Router>
   );
---
+++ manager/src/components/EmployeeCreate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const EmployeeCreate = () => {
+  return (
+    <View>
+      <Text>EmployeeCreate</Text>
+    </View>
+  );
+};
+
+export default EmployeeCreate;
